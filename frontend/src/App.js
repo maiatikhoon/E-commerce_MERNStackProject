94a2705b94a958f315157ad30c0f7c6dad7a6715
atique <alam.atik081@gmail.com>
2024-01-30 18:00:59 +0530

    first commit uptil overall design of ecommerce

---
+++ frontend/src/App.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import logo from './logo.svg';
+import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage'; 
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import CartPage from './pages/CartPage';
+import Checkout from './pages/Checkout';
+import ProductDetailsPage from './pages/ProductDetailsPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>
+  },
+  {
+    path: "/login",
+    element: <LoginPage></LoginPage>,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage></SignupPage>,
+  },
+  {
+    path: "/cart",
+    element: <CartPage></CartPage>
+  },
+  {
+    path: "/checkout",
+    element: <Checkout></Checkout>
+  },
+  {
+    path: "/product-details",
+    element: <ProductDetailsPage></ProductDetailsPage>
+  },
+]);
+
+
+function App() {
+  return (
+    <div className="App">
+        <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/ProductDetailsPage.js
@@ -0,0 +1,14 @@
+import Navbar from "../features/navabar/Navbar";
+import ProductDetails from "../features/product/components/ProductDetails";
+
+function ProductDetailsPage() {
+  return (
+    <div>
+      <Navbar>
+        <ProductDetails></ProductDetails>
+      </Navbar>
+    </div>
+  );
+}
+
+export default ProductDetailsPage;
